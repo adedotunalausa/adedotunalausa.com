78272c3c41b9584297a70e7d3c67b4d493076c8e
Adedotun Alausa <Adedotun@Adedotuns-MacBook-Pro.local>
2020-08-17 01:26:05 +0100

    Add read me file

--- src/components/HomeSection1.jsx
+++ src/components/HomeSection1.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import HeroImg1 from "./images/Adedotun2.png"
+import { motion } from "framer-motion"
 // import HeroImg2 from "./images/Adedotun1.png"
+
+const pageVariants = {
+    in: {
+        opacity: 1,
+        y: 0
+    },
+    out: {
+        opacity: 0,
+        y: "1%"
+    }
+}
+
+const pageTransition = {
+    duration: .8
+}
 
 const HomeSection1 = () => {
 
@@ -10,11 +26,18 @@ const HomeSection1 = () => {
                 <div className="first-section-content">
                     <p className="hello">Hello, I'm Adedotun <span role="img" aria-label="wave">👋🏼</span></p>
                     <h1>Full Stack Web Developer</h1>
-                    <p className="brief-info">
+                    <motion.p
+                        className="brief-info"
+                        initial="out"
+                        animate="in"
+                        exit="out"
+                        variants={pageVariants}
+                        transition={pageTransition}
+                    >
                         I build digital products that meets the expectations of
                         users, help businesses reach their goals and result
                         in an awesome user interaction
-                    </p>
+                    </motion.p>
                     <a className="butn" href="#abilities">More about me</a>
                 </div>
                 <img className="hero-img1" src={HeroImg1} alt="hero-img" />
